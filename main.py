742505e0964a5831a9ca8e0ffbf0626aa1fcc3ac
Invaders <davidwood3@gmail.com>
2024-02-23 20:21:39 -0800

    First commit: Chatbot.py works

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Hi I am a Dave's Streamlit Web App")
+st.subheader("Hi I am a subheader")
+st.header("I am a header")
+st.text("I am a text function")
+st.markdown("**Hello** *World*")
+st.write("HELLO DAVID and Amanda")
+
